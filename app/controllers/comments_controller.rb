c54f6589eb52b8ba982f6a74ceea71d4c20fe831
yuipeee <nooof.0827@gmail.com>
2021-04-19 17:49:05 +0900

    エラー解消-5

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.create(comment_params)
+    @comment = Comment.new(comment_params)
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
